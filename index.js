89339a0e9c42c7b8e8aaa6f28aa4dca420308023
李哈哈 <liyaxingdev@163.com>
2021-08-04 11:32:10 +0800

    配置

--- index.js
+++ index.js
@@ -2,5 +2,18 @@
 import { NativeModules } from 'react-native';
 
 const { RNGeTuistatistic } = NativeModules;
+/**
+ * 初始化
+ */
+export async function init() {
+    return await RNGeTuistatistic.init();
+  }
 
-export default RNGeTuistatistic;
+/**
+ * 
+ * @param {eventId :自定义事件 Id ，用于标识事件的唯一} eventId 
+ */
+  export async function onEvent(eventId) {
+    return await RNGeTuistatistic.onEvent(eventId);
+  }
+// export default RNGeTuistatistic;
